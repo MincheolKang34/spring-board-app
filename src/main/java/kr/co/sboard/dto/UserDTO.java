fd3d0a989aca264933d7446a218a47ad2e20b653
mincheolkang34 <mincheolkang34@gmail.com>
2025-09-29 10:08:36 +0900

    불필요 메서드 삭제

--- src/main/java/kr/co/sboard/dto/UserDTO.java
+++ src/main/java/kr/co/sboard/dto/UserDTO.java
@@ -22,22 +22,4 @@ public class UserDTO {
     private String reg_ip;
     private String reg_date;
     private String leave_date;
-
-    public User toEntity() {
-        return User.builder()
-                .usid(usid)
-                .pass(pass)
-                .us_name(us_name)
-                .nick(nick)
-                .email(email)
-                .hp(hp)
-                .us_role(us_role)
-                .zip(zip)
-                .addr1(addr1)
-                .addr2(addr2)
-                .reg_ip(reg_ip)
-                .reg_date(reg_date)
-                .leave_date(leave_date)
-                .build();
-    }
 }
